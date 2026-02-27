b461594f8ebd9a5381ee52f941a11220531c64d5
ScienceOfDeath <jonathanelkaer@gmail.com>
2026-02-27 10:44:50 +0100

    forgotten to save data in files xD

---
+++ models.py
@@ -0,0 +1,19 @@
+from db import db
+from werkzeug.security import generate_password_hash, check_password_hash
+import uuid
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    public_key = db.Column(db.Text, nullable=True) 
+    encrypted_file_path = db.Column(db.String(256), nullable=True)
+    encrypted_file_key = db.Column(db.Text, nullable=True) 
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
